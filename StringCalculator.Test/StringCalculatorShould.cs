c62ff33bed2304c38f11e5c614a05e82dd4df48f
AaronHdez <aaronhdezalv@hotmail.com>
2021-10-04 16:03:05 +0100

    Red: Test Return_a_number_when_input_is_that_number

---
+++ StringCalculator.Test/StringCalculatorShould.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculator.Test {
+    public class StringCalculatorShould {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Return_0_when_string_is_empty() {
+            //Given
+            string input = "";
+
+            //When
+            int result = StringCalculator.Add(input);
+
+            //Then
+            result.Should().Be(0);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Return_1_when_input_is_1() {
+            const string input = "1";
+
+            var result = StringCalculator.Add(input);
+
+            result.Should().Be(1);
+        }
+
+        [Test]
+        public void Return_a_number_when_input_is_that_number()
+        {
+            const string input = "1";
+
+            var result = StringCalculator.Add(input);
+
+            result.Should().Be(1);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StringCalculator {
+    public static class StringCalculator {
+        public static int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return 0;
+            return int.Parse(input);
+        }
+    }
+}
