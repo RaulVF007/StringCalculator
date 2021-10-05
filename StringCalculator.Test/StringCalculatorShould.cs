4bf532458db39c5357310bd0fd08c38d6e507f07
AaronHdez <aaronhdezalv@hotmail.com>
2021-10-05 13:03:37 +0100

    Refactor:
    StringCalculatorShould:
    
    - Tests have been refactored applying NUnit.TestCase
    - Specific cases have been transformed into parameters on TestCases.

--- StringCalculator.Test/StringCalculatorShould.cs
+++ StringCalculator.Test/StringCalculatorShould.cs
@@ -17,51 +17,34 @@ namespace StringCalculator.Test
             result.Should().Be(0);
         }
 
-        [Test]
-        public void return_1_when_input_is_1()
+        [TestCase("1",1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        public void return_a_number_when_input_is_that_number(string input, int expected)
         {
-            var input = "1";
-
             var result = StringCalculator.Add(input);
 
-            result.Should().Be(1);
+            result.Should().Be(expected);
+        }
+        
+        [TestCase("4,2", 6)]
+        [TestCase("2,2", 4)]
+        [TestCase("3,2", 5)]
+        public void return_addition_when_input_is_two_numbers(string input, int expected)
+        {
+            var result = StringCalculator.Add(input);
+
+            result.Should().Be(expected);
         }
 
-        [Test]
-        public void return_a_number_when_input_is_that_number()
+        [TestCase("4,2,1", 7)]
+        [TestCase("2,2,1", 5)]
+        [TestCase("3,2,4", 9)]
+        public void return_addition_when_input_is_more_than_two_numbers(string input, int expected)
         {
-            var input = "2";
-
             var result = StringCalculator.Add(input);
 
-            result.Should().Be(2);
-        }
-        [Test]
-        public void return_3_when_input_is_1_and_2()
-        {
-            var input = "1,2";
-
-            var result = StringCalculator.Add(input);
-
-            result.Should().Be(3);
-        }
-        [Test]
-        public void return_addition_when_input_is_two_numbers()
-        {
-            var input = "4,2";
-
-            var result = StringCalculator.Add(input);
-
-            result.Should().Be(6);
-        }
-        [Test]
-        public void return_addition_when_input_is_more_than_two_numbers()
-        {
-            var input = "4,2,1";
-
-            var result = StringCalculator.Add(input);
-
-            result.Should().Be(7);
+            result.Should().Be(expected);
         }
 
         [TestCase("1\n2,3\n4,5", 15)]
